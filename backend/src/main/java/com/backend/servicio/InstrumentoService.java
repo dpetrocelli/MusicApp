6ad876c1fa671717956d010a7b361dc367f0a323
aloton <aleromeroar@yahoo.com.ar>
2019-06-20 19:53:13 -0300

    nueva actualizacion

---
+++ backend/src/main/java/com/backend/servicio/InstrumentoService.java
@@ -0,0 +1,31 @@
+package com.backend.servicio;
+
+import com.backend.entidad.Instrumento;
+import com.backend.repositorio.InstrumentoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class InstrumentoService {
+
+    @Autowired
+    InstrumentoRepositorio instrumentoRepositorio;
+
+    public List<Instrumento> obtenerTodos(){
+        List<Instrumento> lista = instrumentoRepositorio.findAll();
+        return lista;
+    }
+
+    public Optional<Instrumento> obtenerPorId(Long id){
+        return instrumentoRepositorio.findById(id);
+    }
+
+    public Optional<Instrumento> obtenerPorNombre(String ni){
+        return instrumentoRepositorio.findByNombreInstrumento(ni);
+    }
+}
