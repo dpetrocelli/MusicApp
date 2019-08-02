0a9cc4f6161e42a740423106856bf90c7e3c69d9
David <Petrocelli>
2019-08-01 23:17:31 -0300

    Rearming Backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# MusicApp2019
-Proyecto Backend + Frontend Jessica Lume - Anto
--- backend/src/test/java/backend/musicapp/BackendApplicationTests.java
+++ backend/src/test/java/backend/musicapp/BackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MusicappApplicationTests {
+public class BackendApplicationTests {
 
     @Test
     public void contextLoads() {
