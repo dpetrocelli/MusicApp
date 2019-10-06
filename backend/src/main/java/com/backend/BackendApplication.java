f37f7e27300b69a4e5b65cb9a32bf5cdbd75cca0
aloton <aleromeroar@yahoo.com.ar>
2019-10-06 14:56:39 -0300

    Permitir CORS desde cualquier origen para cualquier metodo, globalmente

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,16 +1,26 @@
 package com.backend;
 
-
 import com.backend.singleton.ConfiguradorSingleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
-
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		//SpringApplication.run(BackendApplication.class, args);
