7e44dd579a0f886dc6592ee26e79e1f4cb19fa28
David <Petrocelli>
2019-11-02 19:35:37 -0300

    Zona working

--- frontend/src/app/zonas/nueva-zona.component.ts
+++ frontend/src/app/zonas/nueva-zona.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ZonaService } from '../servicios/zona.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nueva-zona',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaZonaComponent implements OnInit {
 
-  constructor() { }
+  form: any = {};
+  actualizado = false;
+  falloActualizacion = false;
+  msjFallo = '';
+  msjOK = '';
+
+  fallaInit = false;
+
+  constructor(private zonaService: ZonaService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    
   }
 
+  onCreate(): void {
+    
+    this.zonaService.crear(this.form).subscribe(data => {
+        this.actualizado = true;
+        this.falloActualizacion = false;
+        this.msjOK = data.mensaje;
+      },
+      (err: any) => {
+        this.actualizado = false;
+        this.falloActualizacion = true;
+        this.msjFallo = err.error.mensaje;
+      }
+    );
+  }
+
+  volver(): void {
+    window.history.back();
+  }
 }
+
